a0c7afe899def82554d6be9b625018b935d1783f
harits <haritsdeveloper@gmail.com>
2025-10-07 12:56:01 +0700

    IfElseIfExample

---
+++ praktikum03-05/IfElseIfExample.java
@@ -0,0 +1,16 @@
+public class IfElseIfExample {
+  public static void main(String[] args) {
+    int nilai = 78;
+
+    if (nilai >= 85) {
+        System.out.println( "Grade A" );
+    } else if(nilai >= 70) { 
+        System.out.println( "Grade B" );
+    } else if (nilai >= 55) { 
+        System.out.println( "Grade C" );
+    } else {
+    System.out.println( "Grade D" );
+    }
+  }
+    
+} 
