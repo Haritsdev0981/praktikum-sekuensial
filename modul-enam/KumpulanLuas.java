e9e8abf621dca4bbb25588e2301f05be36ab5c22
harits <haritsdeveloper@gmail.com>
2025-11-04 13:24:15 +0700

    parameter

--- modul-enam/KumpulanLuas.java
+++ modul-enam/KumpulanLuas.java
@@ -1,15 +1,17 @@
 public class KumpulanLuas {
-    public static String LuasSegitiga() {
+    public static String LuasSegitiga(int alas, int tinggi) {
         //perhitungan luas segitiga
-        int alas, tinggi;
+        // int alas, tinggi;
         double luasSegitiga;
-        alas = 10;
-        tinggi = 5;
+        // alas = 10;
+        // tinggi = 5;
         luasSegitiga = 0.5 * alas * tinggi;
         return "Luas segitiga= " + luasSegitiga;
     }
 
     public static void main(String[] args) {
-        System.out.println(LuasSegitiga());
+        System.out.println(LuasSegitiga(10, 5));
+        System.out.println(LuasSegitiga(6, 9));
+        System.out.println(LuasSegitiga(8, 8));
     }
 }
