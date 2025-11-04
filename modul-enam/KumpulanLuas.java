64b485f85afffab5eca8f21622f4904a2d965b39
harits <haritsdeveloper@gmail.com>
2025-11-04 13:14:26 +0700

    finish

--- modul-enam/KumpulanLuas.java
+++ modul-enam/KumpulanLuas.java
@@ -1,5 +1,5 @@
 public class KumpulanLuas {
-    public static void main(String[] args) {
+    public static void LuasSegitiga() {
         //perhitungan luas segitiga
         int alas, tinggi;
         double luasSegitiga;
@@ -7,5 +7,9 @@ public class KumpulanLuas {
         tinggi = 5;
         luasSegitiga = 0.5 * alas * tinggi;
         System.out.println("Luas segitiga= " + luasSegitiga);
+    }
+
+    public static void main(String[] args) {
+        LuasSegitiga();
     }
 }
